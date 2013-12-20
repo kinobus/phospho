fa7109ca94fa6b1b1f8b1dc235d43d3bdb5d15a1
Nick Robin <nrobin24@uw.edu>
2013-12-20 13:27:25 -0800

    Add AngularStrap and dropdown.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('phosphoApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  '$strap.directives'
 ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -2,5 +2,17 @@
 
 angular.module('phosphoApp')
   .controller('ShareCtrl', function ($scope, Buildkinome) {
+
+    $scope.dropdown = [
+      {text: 'Another action', href: '#anotherAction'},
+      {text: 'Something else here', click: "$alert('working ngClick!')"},
+      {divider: true},
+      {text: 'Separated link', href: '#',
+        submenu: [
+          {text: 'Second level link', href: '#'},
+          {text: 'Second level link 2', href: '#'}
+        ]}
+      ];
+
     $scope.kinome = new Buildkinome.build([0.5, 0.3, 0.2]);
   });
