c95170f6732eea77125bb806a802921dd272a290
Nick Robin <nrobin24@uw.edu>
2013-12-20 14:54:55 -0800

    Add ui.bootstrap.

--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -3,4 +3,10 @@
 angular.module('phosphoApp')
   .controller('ShareCtrl', function ($scope, Buildkinome) {
     $scope.kinome = new Buildkinome.build([0.5, 0.3, 0.2]);
+
+    $scope.items = [
+      "The first choice!",
+      "And another choice for you.",
+      "but wait! A third!"
+    ];
   });
