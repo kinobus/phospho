4bce3484d3cda54bdb3a8cba47d993bcda3fc96c
Nick Robin <nrobin24@uw.edu>
2013-12-20 15:43:25 -0800

    Add dropdown toggle.

--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -4,9 +4,29 @@ angular.module('phosphoApp')
   .controller('ShareCtrl', function ($scope, Buildkinome) {
     $scope.kinome = new Buildkinome.build([0.5, 0.3, 0.2]);
 
-    $scope.items = [
-      "The first choice!",
-      "And another choice for you.",
-      "but wait! A third!"
-    ];
+    $scope.Interactomes = [{
+      "ID": 1,
+      "Links": "itemone",
+      "Name": "Item One"
+    }, {
+      "ID": 2,
+      "Links": "itemtwo",
+      "Name": "Item Two"
+    }, {
+      "ID": 3,
+      "Links": "itemthree",
+      "Name": "Item Three"
+    }, {
+      "ID": 5,
+      "Links": "whereisfour",
+      "Name": "Item Five"
+    }];
+
+    $scope.loadedInteractome = 'nothing loaded yet';
+    $scope.kinomelinks = 'empty link';
+
+    $scope.loadInteractome = function(name, links) {
+      $scope.loadedInteractome = name;
+      $scope.kinomelinks = links;
+    }
   });
