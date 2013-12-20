ff2686dae23b3b70f0a31ffcf76045f4f8d4e31b
Nick Robin <nrobin24@uw.edu>
2013-12-20 14:34:26 -0800

    Updated AngularStrap dropdown in share.

--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -2,17 +2,20 @@
 
 angular.module('phosphoApp')
   .controller('ShareCtrl', function ($scope, Buildkinome) {
+    //initialize
+    $scope.ksi = 'PhosphoNET';
 
-    $scope.dropdown = [
-      {text: 'Another action', href: '#anotherAction'},
-      {text: 'Something else here', click: "$alert('working ngClick!')"},
-      {divider: true},
-      {text: 'Separated link', href: '#',
-        submenu: [
-          {text: 'Second level link', href: '#'},
-          {text: 'Second level link 2', href: '#'}
-        ]}
-      ];
+    //dropdown menus to choose ms2dataset and ksi
+    $scope.ms2Dropdown = [
+      {text: 'dasatinib vs dmso', click: "$alert('dasatinib')"},
+      {text: 'ibrutinib vs dmso', click: "$alert('ibrutinib')"}
+    ];
 
+    $scope.ksiDropdown = [
+      {text: 'PhosphoNET', click: "$scope.ksi('PhosphoNET')"},
+      {text: 'Cell Signaling', click: "$scope.ksi('Cell Signaling')"}
+    ];
+
+    //build the kinome for display using the Buildkinome service
     $scope.kinome = new Buildkinome.build([0.5, 0.3, 0.2]);
   });
