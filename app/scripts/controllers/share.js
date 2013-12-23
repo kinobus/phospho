e487ab660b88b1c624679a40dec256e9483a00a0
Nick Robin <nrobin24@uw.edu>
2013-12-23 10:50:59 -0800

    Update for real value in firebase

--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -5,13 +5,23 @@ angular.module('phosphoApp')
 
     $scope.interactomes = $firebase(new Firebase('https://phospho.firebaseio.com/interactomes'));
 
-    $scope.kinome = new Buildkinome.build([0.5, 0.3, 0.2]);
-
-    $scope.loadedInteractome = 'nothing loaded yet';
+    $scope.loadedInteractome = 'blank interactome';
     $scope.kinomelinks = 'empty link';
 
-    $scope.loadInteractome = function(title, links) {
+    $scope.selectInteractome = function(title, links) {
       $scope.loadedInteractome = title;
       $scope.kinomelinks = links;
     };
+
+    $scope.ms2datasets = $firebase(new Firebase('https://phospho.firebaseio.com/ms2datasets'));
+
+    $scope.loadedMs2dataset = 'blank dataset';
+    $scope.ms2means = 'empty means';
+
+    $scope.selectMs2dataset = function(title, data) {
+      $scope.loadedMs2dataset = title;
+      $scope.ms2means = data;
+    };
+
+    $scope.kinome = new Buildkinome.build([0.5, 0.3, 0.2]);
   });
