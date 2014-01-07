5e7d94a690189e32544655d72364a4700f66ee3f
Nick Robin <nrobin24@uw.edu>
2014-01-07 13:20:14 -0800

    Added gradient background.

--- app/scripts/directives/dmEdit.js
+++ app/scripts/directives/dmEdit.js
@@ -3,7 +3,7 @@
 angular.module('phosphoApp')
   .directive('dmEdit', function () {
     //constants
-    var width = 510,
+    var width = 516,
       height = 600;
 
     return {
@@ -58,21 +58,55 @@ angular.module('phosphoApp')
               .attr('d','M24.5,18.5 L96.5,18.5 C103.127,18.5 108.5,23.873 108.5,30.5 L108.5,42.5 C108.5,49.127 103.127,54.5 96.5,54.5 L24.5,54.5 C17.873,54.5 12.5,49.127 12.5,42.5 L12.5,30.5 C12.5,23.873 17.873,18.5 24.5,18.5 z')
               .attr('fill','#F3B73E')
               .attr('stroke', '#000')
-              .attr('stroke-width', '2px');
+              .attr('stroke-width', '3px');
 
           defs.append('svg:path')
               .attr('id', 'event-node')
               .attr('d','M0.5,12.5 L120.5,12.5 L120.5,60.5 L0.5,60.5 z')
-              .attr('fill','#B5D5C6')
+              .attr('fill','#2AFFFF')
               .attr('stroke', '#000')
-              .attr('stroke-width', '2px');
+              .attr('stroke-width', '3px');
 
           defs.append('svg:path')
               .attr('id', 'pathway-node')
               .attr('d','M0.5,12.5 L120.5,12.5 L120.5,60.5 L0.5,60.5 z')
-              .attr('fill','#DE3265')
+              .attr('fill','#55FF6D')
               .attr('stroke', '#000')
-              .attr('stroke-width', '2px');
+              .attr('stroke-width', '3px');
+
+          var gradient = defs.append('svg:linearGradient')
+              .attr('id', 'gradient')
+              .attr('x1', '0%')
+              .attr('y1', '0%')
+              .attr('x2', '0%')
+              .attr('y2', '100%')
+              .attr('spreadMethod', 'pad');
+
+          gradient.append('svg:stop')
+              .attr('offset', '0%')
+              .attr('stop-color', '#3F91A3')
+              .attr('stop-opacity', 1);
+
+          gradient.append('svg:stop')
+              .attr('offset', '10%')
+              .attr('stop-color', '#6A6A6A')
+              .attr('stop-opacity', 1);
+
+          gradient.append('svg:stop')
+              .attr('offset', '90%')
+              .attr('stop-color', '#6A6A6A')
+              .attr('stop-opacity', 1);
+
+          gradient.append('svg:stop')
+              .attr('offset', '100%')
+              .attr('stop-color', '#FF2A85')
+              .attr('stop-opacity', 1);
+
+          //draw background
+          svg.append('svg:rect')
+            .attr('width', width)
+            .attr('height', height)
+            .style('fill', 'url(#gradient)');
 
           var nodes = newVal.nodes,
             links = newVal.links;
@@ -100,8 +134,8 @@ angular.module('phosphoApp')
             .style('marker-end', function(d) { return 'url(#' + d.type + '-arrow)'; });
 
           path.style('stroke-width', '4px')
-            .style('stroke', function(d) { 
-              if (d.type === 'activate') { 
+            .style('stroke', function(d) {
+              if (d.type === 'activate') {
                 return '#00FF00';
               }
               else if (d.type === 'inhibit') {
@@ -123,7 +157,7 @@ angular.module('phosphoApp')
           node.append('svg:text')
             .attr('x', 60)
             .attr('y', 42)
-            .attr('font-family', 'sans-serif')
+            .attr('font-family', 'HelveticaNeue-Bold')
             .attr('font-size', '18px')
             .attr('text-anchor', 'middle')
             .text(function(d) { return d.label; });
