5660d63d653e77a249305661e0ca1fd3dda502cd
Nick Robin <nrobin24@uw.edu>
2014-01-14 17:33:08 -0800

    Update test specs.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,8 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  //tests start here
+  it('should start with a null value for selectedItem', function () {
+    expect(scope.selectedItem).toBe(null);
   });
 });
